c48ad59064dd24ccf6609a90b41b0aaafe5ef577
Panatore <97985464+Panatore@users.noreply.github.com>
2022-11-01 17:12:25 +0100

    Change in Jupyter notebook and created the streamlit webapp

--- webapp.py
+++ webapp.py
@@ -1,7 +1,22 @@
 import streamlit as st
 from keras.models import load_model
+import tensorflow as tf
+from keras.utils import load_img, img_to_array
+import numpy as np
+
+def load_image(img_path):
+
+
+    img = load_img(img_path, target_size=(224, 224))
+    img_tensor = img_to_array(img)                    # (height, width, channels)
+    img_tensor = np.expand_dims(img_tensor, axis=0)         # (1, height, width, channels), add a dimension because the model expects this shape: (batch_size, height, width, channels)
+    img_tensor /= 255.  
+
+    return img_tensor
 
 # model = load_model(r"C:\Users\danie\Documentos\GitHub\RoomsClassifier\Models\model_transfer_learning\saved_model.pb")
+model = load_model('Models\model_transfer_learning\model_transfer_learning.h5')
+class_names = ['Bathroom', 'Bedroom', 'House Map', 'Kitchen', 'Living Room']
 
 st.title("Room Classifier")
 
@@ -11,6 +26,9 @@ if image_uploaded is not None:
 
 if st.button("Predict"):
     if image_uploaded is not None:
-       prediction =  model.predict(image_uploaded)
-       st.write(prediction)
+       img_tensor = load_image(image_uploaded)
+       prediction =  model.predict(img_tensor)
+       st.write(class_names[np.argmax(prediction)])
+    else:
+        st.warning("Upload a photo before making predictions")
        
