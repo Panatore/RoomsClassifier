4ed2264c07056f9bb4e3879b97ceb78654c42b88
Panatore <97985464+Panatore@users.noreply.github.com>
2022-12-30 12:33:11 +0100

    New data and streamlit app
    Add new data in the training and testing a new streamlit app to show the concept idea

---
+++ fine_tuning.py
@@ -0,0 +1,74 @@
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.models import Model
+
+# Cargar el modelo preentrenado
+base_model = VGG16(weights='imagenet', include_top=False, input_shape=(224,224,3))
+
+# Congelar las capas del modelo
+for layer in base_model.layers:
+  layer.trainable = False
+
+# Añadir una capa completamente conectada al final
+x = base_model.output
+x = Flatten()(x)
+x = Dense(1024, activation='relu')(x)
+x = Dropout(0.5)(x)
+predictions = Dense(10, activation='softmax')(x)
+
+# Crear un modelo basado en el modelo preentrenado
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# Compilar el modelo
+model.compile(optimizer=SGD(lr=0.001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Preparar los datos de entrenamiento y validación
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        'data/train',
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_directory(
+        'data/validation',
+        target_size=(224, 224),
+        batch_size=32,
+        class_mode='categorical')
+
+# Entrenar el modelo
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=train_generator.samples/train_generator.batch_size ,
+      epochs=10,
+      validation_data=validation_generator,
+      validation_steps=validation_generator.samples/validation_generator.batch_size,
+      verbose=1)
+
+# Descongelar todas las capas del modelo
+for layer in base_model.layers:
+  layer.trainable = True
+
+# Compilar el modelo con un learning rate bajo
+model.compile(optimizer=SGD(lr=0.0001, momentum=0.9), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Entrenar el modelo
+history = model.fit_generator(
+      train_generator,
+      steps_per_epoch=train_generator.samples/train_generator.batch_size ,
+      epochs=10,
+      validation_data=validation_generator,
+      validation_steps=validation_generator.samples/validation_generator.batch_size,
+      verbose=1)
+
+# Guardar el modelo entrenado
+model.save('fine_tuned_model.h5')
+
