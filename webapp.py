1d9e1a723348cb59210c210bb9c4e810c95772e6
Panatore <97985464+Panatore@users.noreply.github.com>
2023-01-08 19:40:33 +0100

    Creating the webapp using flask
    I have limitations using streamlit to create the webapp i am thinking so I'm change to flask framework.

--- webapp.py
+++ webapp.py
@@ -14,32 +14,76 @@ def load_image(img_path):
 
     return img_tensor
 
+def get_inputs(page):
+    if page == "Create your ad":
+        operation = st.radio("Operation:",["Rent","Sale"])
+
+        price = st.number_input("Price",min_value=0,step=50)
+        city =  st.text_input("City")
+        road_name =  st.text_input("Road name")
+        description = st.text_area("Description: ",height=150, max_chars= 500)
+        images_uploaded = st.file_uploader("Upload your Images here",type=["jpg","png","jpeg"],accept_multiple_files=True,)
+        if images_uploaded is not None:
+            st.image(images_uploaded)
+
+        if st.button("Create your add or update it"):
+            if images_uploaded is not None and price is not None and city is not None and road_name is not None and description is not None:
+                predictions = []
+                for image in images_uploaded:
+                    img_tensor = load_image(image)
+                    prediction =  model.predict(img_tensor)
+                    predictions.append(class_names[np.argmax(prediction)])
+                st.write(predictions)
+                return operation,price, city, road_name, description, images_uploaded, predictions
+            else:
+                st.warning("Fill in all the fields in order to create your advertisement.")
+
+    elif page == "Your add":
+        operation,price, city, road_name, description, images_uploaded, predictions = get_inputs("Create your app")
+        return operation,price, city, road_name, description, images_uploaded, predictions
+
+
 # model = load_model(r"C:\Users\danie\Documentos\GitHub\RoomsClassifier\Models\model_transfer_learning\saved_model.pb")
+#Load the model and the name of the classes
 model = load_model('Models\model_transfer_learning\model_transfer_learning.h5')
 class_names = ['Bathroom', 'Bedroom', 'House Map', 'Kitchen', 'Living Room']
+#Create this varibale in case the add is create
+add_created = False
 
-st.title("Place your real estate ad")
+# Create a sidebar menú
+pages = st.sidebar.selectbox("Selecciona el menú", ["Create your ad", "Your ad"])
+if pages == "Create your ad":
+    st.title("Place your real estate ad")
 
-operation = st.radio("Operation:",["Rent","Sale"])
+    get_inputs("Create your ad")
+    
+    # operation = st.radio("Operation:",["Rent","Sale"])
 
-price = st.number_input("Price",min_value=0,step=50)
+    # price = st.number_input("Price",min_value=0,step=50)
 
-city =  st.text_input("City")
+    # city =  st.text_input("City")
 
-road_name =  st.text_input("Road name")
+    # road_name =  st.text_input("Road name")
 
-images_uploaded = st.file_uploader("Upload your Images here",type=["jpg","png"],accept_multiple_files=True,)
-if images_uploaded is not None:
-    st.image(images_uploaded)
+    # description = st.text_area("Description: ",height=150, max_chars= 500)
 
-if st.button("Create your ad"):
-    if images_uploaded is not None:
-        predictions = []
-        for image in images_uploaded:
-            img_tensor = load_image(image)
-            prediction =  model.predict(img_tensor)
-            predictions.append(class_names[np.argmax(prediction)])
-        st.write(predictions)
-    else:
-        st.warning("Upload a photo before making predictions")
-       
+    # images_uploaded = st.file_uploader("Upload your Images here",type=["jpg","png","jpeg"],accept_multiple_files=True,)
+    # if images_uploaded is not None:
+    #     st.image(images_uploaded)
+
+    # if st.button("Create your add or update it"):
+    #     if images_uploaded is not None and price is not None and city is not None and road_name is not None and description is not None:
+    #         add_created = True
+    #         predictions = []
+    #         for image in images_uploaded:
+    #             img_tensor = load_image(image)
+    #             prediction =  model.predict(img_tensor)
+    #             predictions.append(class_names[np.argmax(prediction)])
+    #         st.write(predictions)
+    #     else:
+    #         st.warning("Fill in all the fields in order to create your advertisement.")
+
+else:
+    get_inputs("Your add")
+    st.info("Info")
+
