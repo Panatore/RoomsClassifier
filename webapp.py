023b9f55dc691c2eb916ac6bd7fb961a59b1fce1
Panatore <97985464+Panatore@users.noreply.github.com>
2023-01-24 00:43:01 +0100

    Changes in the Webapp

--- webapp.py
+++ webapp.py
@@ -3,8 +3,8 @@ from keras.models import load_model
 import tensorflow as tf
 from keras.utils import load_img, img_to_array
 import numpy as np
+# Function to load the image in the format that the model can make the predictions and normalize it.
 def load_image(img_path):
-
 
     img = load_img(img_path, target_size=(224, 224))
     img_tensor = img_to_array(img)                    # (height, width, channels)
@@ -13,33 +13,12 @@ def load_image(img_path):
 
     return img_tensor
 
-# def get_inputs(page):
-#     if page == "Create your ad":
-#         operation = st.radio("Operation:",["Rent","Sale"])
-
-#         price = st.number_input("Price",min_value=0,step=50)
-#         city =  st.text_input("City")
-#         road_name =  st.text_input("Road name")
-#         description = st.text_area("Description: ",height=150, max_chars= 500)
-#         images_uploaded = st.file_uploader("Upload your Images here",type=["jpg","png","jpeg"],accept_multiple_files=True,)
-#         if images_uploaded is not None:
-#             st.image(images_uploaded)
-
-#         if st.button("Create your add or update it"):
-#             if images_uploaded is not None and price is not None and city is not None and road_name is not None and description is not None:
-#                 predictions = []
-#                 for image in images_uploaded:
-#                     img_tensor = load_image(image)
-#                     prediction =  model.predict(img_tensor)
-#                     predictions.append(class_names[np.argmax(prediction)])
-#                 st.write(predictions)
-#                 return operation,price, city, road_name, description, images_uploaded, predictions
-#             else:
-#                 st.warning("Fill in all the fields in order to create your advertisement.")
-
-#     elif page == "Your add":
-#         operation,price, city, road_name, description, images_uploaded, predictions = get_inputs("Create your app")
-#         return operation,price, city, road_name, description, images_uploaded, predictions
+#Create a function to load the model of the classifier and store in the picle memory
+# @st.experimental_singleton
+# def load_model():
+#     model = load_model('Models\model_transfer_learning\model_transfer_learning.h5')
+#     return model
+model = load_model('Models\model_transfer_learning\model_transfer_learning.h5')
 
 
 # model = load_model(r"C:\Users\danie\Documentos\GitHub\RoomsClassifier\Models\model_transfer_learning\saved_model.pb")
@@ -47,14 +26,14 @@ def load_image(img_path):
 model = load_model('Models\model_transfer_learning\model_transfer_learning.h5')
 class_names = ['Bathroom', 'Bedroom', 'House Map', 'Kitchen', 'Living Room']
 #Create this varibale in case the add is create
-add_created = False
+
 
 # Create a sidebar menú
 pages = st.sidebar.selectbox("Selecciona el menú", ["Create your ad", "Your ad"])
 if pages == "Create your ad":
+    #Create this variable in case the add is created
+    add_created = False
     st.title("Place your real estate ad")
-
-    # get_inputs("Create your ad")
     
     operation = st.radio("Operation:",["Rent","Sale"])
 
@@ -67,9 +46,10 @@ if pages == "Create your ad":
     description = st.text_area("Description: ",height=150, max_chars= 500)
 
     images_uploaded = st.file_uploader("Upload your Images here",type=["jpg","png","jpeg"],accept_multiple_files=True,)
+    #If there is images show them
     if images_uploaded is not None:
         st.image(images_uploaded)
-
+    #If all fields are fullfil create the add and make the predictions
     if st.button("Create your add or update it"):
         if images_uploaded is not None and price is not None and city is not None and road_name is not None and description is not None:
             add_created = True
@@ -79,10 +59,31 @@ if pages == "Create your ad":
                 prediction =  model.predict(img_tensor)
                 predictions.append(class_names[np.argmax(prediction)])
             st.write(predictions)
+
+            #Declare the variable in the session state to not lose after change to the other page
+            st.session_state['operation'] = operation
+            st.session_state['price'] = price
+            st.session_state['city'] = city
+            st.session_state['road_name'] = road_name
+            st.session_state['description'] = description
+            st.session_state['predictions'] = predictions
+            st.session_state['images_upload'] = images_uploaded
+            st.session_state['add_created'] = add_created
         else:
             st.warning("Fill in all the fields in order to create your advertisement.")
 
-else:
-    # get_inputs("Your add")
-    st.info("Info")
+elif pages == "Your ad":
+    if st.session_state['add_created']  == True:
+        operation = st.session_state['operation']
+        price = st.session_state['price'] 
+        city = st.session_state['city']
+        road_name = st.session_state['road_name']
+        description = st.session_state['description'] 
+        predictions = st.session_state['predictions'] 
+        images_upload = st.session_state['images_upload']
+        st.header(f"{operation} a real state in {city}, {road_name}")
+        st.subheader(description)
+    else:
+        st.info("You should create the add first")
+    
 
